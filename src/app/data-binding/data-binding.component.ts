c45f13af09d0c10c4a8e757882c50a00a6c96be6
Bruno Costa <brunoarruda697@gmail.com>
2020-11-10 18:11:00 -0300

    adicionando teste de two-way binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
   numero: number;
+  nome: string;
   constructor() { }
   ngOnInit() {
   }
